c16b342c27f3056380bfe0d80dbbb597cadd73b8
Bladimir Almanzar <bladdy34@gmail.com>
2025-10-20 17:57:50 -0600

    update urls apis

--- SIC/SIC.Frontend/wwwroot/js/invitations.js
+++ SIC/SIC.Frontend/wwwroot/js/invitations.js
@@ -78,7 +78,7 @@ function validarBotonEnviar() {
 // Función para generar QR
 // ==============================
 function fillQRCodeImage(codigo_inv, codigo_evento) {
-    var qrUrl = `https://localhost:7141/api/Invitations/qr?codigo=${codigo_inv}&evento=${codigo_evento}`;
+    var qrUrl = `http://invboxv-app.com/api/Invitations/qr?codigo=${codigo_inv}&evento=${codigo_evento}`;
 
     fetch(qrUrl)
         .then(response => response.json())
@@ -106,7 +106,7 @@ function fillQRCodeImage(codigo_inv, codigo_evento) {
 function obtenerDatosInvitacion(codigo) {
     // Deshabilitar inicialmente el botón
     boton.disabled = true;
-    var apiUrl = `https://localhost:7141/api/Invitations/byCode/${codigo}`;
+    var apiUrl = `http://invboxv-app.com/api/Invitations/byCode/${codigo}`;
 
     fetch(apiUrl)
         .then(response => {
@@ -202,7 +202,7 @@ function sendRespuesta() {
         Mensaje: mensaje
     };
 
-    fetch("https://localhost:7141/api/Invitations/confirm", {
+    fetch("http://invboxv-app.com/api/Invitations/confirm", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(payload)
