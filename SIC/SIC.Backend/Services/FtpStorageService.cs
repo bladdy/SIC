52dbdc95848042518194a32c608c46449c69dbd6
Bladimir Almanzar <bladdy34@gmail.com>
2026-02-03 18:08:41 -0600

    ftp

--- SIC/SIC.Backend/Services/FtpStorageService.cs
+++ SIC/SIC.Backend/Services/FtpStorageService.cs
@@ -20,13 +20,13 @@ public class FtpStorageService
     private FtpClient CreateClient()
     {
         //Cambiar a FtpLocal cuando sea local
-        var host = _configuration["FtpLocal:Host"];
-        var username = _configuration["FtpLocal:Username"];
-        var password = _configuration["FtpLocal:Password"];
+        var host = _configuration["Ftp:Host"];
+        var username = _configuration["Ftp:Username"];
+        var password = _configuration["Ftp:Password"];
         var client = new FtpClient(host)
         {
             Credentials = new System.Net.NetworkCredential(username, password),
-            Port = int.Parse(_configuration["FtpLocal:Port"] ?? "21")
+            Port = int.Parse(_configuration["Ftp:Port"] ?? "21")
         };
         client.Connect();
         return client;
@@ -94,7 +94,7 @@ public class FtpStorageService
             true
         );
 
-        var baseUrl = _configuration["FtpLocal:UrlBase"];
+        var baseUrl = _configuration["Ftp:UrlBase"];
         return $"{baseUrl}/{folder}/{webpFileName}";
     }
 
@@ -125,7 +125,8 @@ public class FtpStorageService
         var baseUrl = _configuration["FtpLocal:UrlBase"];
         return Task.FromResult($"{baseUrl}/{folder}/{fileName}");
     }*/
-   //ToDo: Crear un metodo que diga si o no se borro el archivo
+
+    //ToDo: Crear un metodo que diga si o no se borro el archivo
     public Task DeleteFileAsync(string folder, string fileName)
     {
         using var client = CreateClient();
