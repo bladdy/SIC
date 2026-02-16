130cf2b13133c54ecc47c0f9b2ab990cce05111c
Bladimir Almanzar <bladdy34@gmail.com>
2026-02-16 17:25:41 -0600

    port

--- SIC/SIC.Frontend/Program.cs
+++ SIC/SIC.Frontend/Program.cs
@@ -14,7 +14,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 // Detectar si estamos corriendo dentro de Docker
 var isDocker = Environment.GetEnvironmentVariable("DOTNET_RUNNING_IN_CONTAINER") == "true";
 
-string backendUrl = "https://localhost:7141/";//"http://localhost:5000/";
+string backendUrl = "http://localhost:5000/";//"https://localhost:7141/";
 /*if (isDocker)
 {
     // Contenedores Docker: usar nombre del servicio y puerto interno
